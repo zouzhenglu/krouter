15ad5156d9cbf792afe24125979ed4aede8d1258
zouzhenglu <346057186@qq.com>
2019-10-14 17:34:57 +0800

    appLink

--- module-course/src/main/java/com/kzl/module_course/page/ScrollingActivity.kt
+++ module-course/src/main/java/com/kzl/module_course/page/ScrollingActivity.kt
@@ -1,8 +1,11 @@
 package com.kzl.module_course.page
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.kzl.module_course.R
 import com.kzl.module_course.topicService
 import kotlinx.android.synthetic.main.activity_scrolling.*
@@ -13,10 +16,38 @@ class ScrollingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrolling)
         setSupportActionBar(toolbar)
+
+        handleIntent(intent)
+
         fab.setOnClickListener { view ->
+            handleIntent(intent)
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
             topicService.sayHello(this)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val appLinkAction = intent.action
+        val appLinkData: Uri? = intent.data
+
+        Log.d("LINK", "appLinkAction:${appLinkAction}")
+        Log.d("LINK", "appLinkData:${appLinkData}")
+
+        if (Intent.ACTION_VIEW == appLinkAction) {
+            appLinkData?.lastPathSegment?.also { recipeId ->
+                Uri.parse("content://com.recipe_app/recipe/")
+                    .buildUpon()
+                    .appendPath(recipeId)
+                    .build().also { appData ->
+                        Log.d("LINK", "showRecipe:${appData}")
+                    }
+            }
         }
     }
 }
--- module-home/src/main/java/com/kzl/module_home/HomeActivity.kt
+++ module-home/src/main/java/com/kzl/module_home/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.kzl.module_home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,12 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         findViewById<View>(R.id.button).setOnClickListener {
+            startActivity(
+                Intent.parseUri(
+                    "http://www.zhiyecn.com/recipe/222222",
+                    Intent.URI_ANDROID_APP_SCHEME
+                )
+            )
             topicService.sayHello(this)
         }
         supportFragmentManager.commit {
